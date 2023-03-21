147c807d6805a6bd6cc446a24003e62c38e625a8
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-03-21 11:04:22 +0100

    feat(tuif-dom): rectangle

---
+++ Tuif/Dom/Rect.cs
@@ -0,0 +1,24 @@
+namespace Tuif.Dom;
+
+public class Rect : Node
+{
+
+    public char FillChar = '█';
+    public Color FillColor = Color.DefaultForeground;
+    public Rect(uint width , uint height) : base(width, height)
+    {
+    }
+
+    public override void Render(Buffer buff) 
+    {
+        for (uint y = 0; y < _height; y++)
+        {
+            uint index = buff.GetIndex(_posX, _posY + y);
+            for (int x = 0; x < _width; x++)
+            {
+                buff.Data[index + x] = FillChar;
+                buff.Foreground[index + x] = FillColor;
+            }
+        }
+    }
+}
