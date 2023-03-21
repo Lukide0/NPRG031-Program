019e16970426dde9d84219d72454662fbaa00023
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-03-21 11:04:03 +0100

    feat(tuif-dom): frame

---
+++ Tuif/Dom/Border.cs
@@ -0,0 +1,153 @@
+namespace Tuif.Dom;
+
+public static class BorderChar 
+{
+    public static char MergeChars(char oldChar, char newChar)
+    {
+        Border a = LookupBorder(oldChar), b = LookupBorder(newChar);
+
+        if (a == Border.Unknown)
+        {
+            return newChar;
+        }
+        else
+        {
+            return LookupChar(a | b);
+        }
+    }
+
+    public static char LookupChar(Border border)
+    {
+        switch (border)
+        {
+            case Border.LeftSingle | Border.RightSingle: return '─';
+            case Border.TopSingle | Border.DownSingle: return '│';
+            case Border.RightSingle | Border.DownSingle: return '┌';
+            case Border.LeftSingle | Border.DownSingle: return '┐';
+            case Border.TopSingle | Border.RightSingle: return '└';
+            case Border.LeftSingle | Border.TopSingle: return '┘';
+            case Border.TopSingle | Border.DownSingle | Border.RightSingle: return '├';
+            case Border.TopSingle | Border.DownSingle | Border.LeftSingle: return '┤';
+            case Border.LeftSingle | Border.RightSingle | Border.DownSingle: return '┬';
+            case Border.LeftSingle | Border.RightSingle | Border.TopSingle: return '┴';
+            case Border.LeftSingle | Border.RightSingle | Border.TopSingle | Border.DownSingle: return '┼';
+
+            case Border.LeftDouble | Border.RightDouble: return '═';
+            case Border.TopDouble | Border.DownDouble: return '║';
+            case Border.RightDouble | Border.DownDouble: return '╔';
+            case Border.LeftDouble | Border.DownDouble: return '╗';
+            case Border.TopDouble | Border.RightDouble: return '╚';
+            case Border.LeftDouble | Border.TopDouble: return '╝';
+            case Border.TopDouble | Border.DownDouble | Border.RightDouble: return '╠';
+            case Border.TopDouble | Border.DownDouble | Border.LeftDouble: return '╣';
+            case Border.LeftDouble | Border.RightDouble | Border.DownDouble: return '╦';
+            case Border.LeftDouble | Border.RightDouble | Border.TopDouble: return '╩';
+            case Border.LeftDouble | Border.RightDouble | Border.TopDouble | Border.DownDouble: return '╬';
+
+            case Border.RightSingle | Border.DownDouble: return '╓';
+            case Border.RightDouble | Border.DownSingle: return '╒';
+
+            case Border.LeftDouble | Border.DownSingle: return '╕';
+            case Border.LeftSingle | Border.DownDouble: return '╖';
+
+            case Border.TopSingle | Border.RightDouble: return '╘';
+            case Border.TopDouble | Border.RightSingle: return '╙';
+
+            case Border.LeftDouble | Border.TopSingle: return '╛';
+            case Border.LeftSingle | Border.TopDouble: return '╜';
+
+            case Border.TopSingle | Border.DownSingle | Border.RightDouble: return '╞';
+            case Border.TopDouble | Border.DownDouble | Border.RightSingle: return '╟';
+
+            case Border.LeftDouble | Border.TopSingle | Border.DownSingle: return '╡';
+            case Border.LeftSingle | Border.TopDouble | Border.DownDouble: return '╢';
+
+            case Border.LeftDouble | Border.RightDouble | Border.DownSingle: return '╤';
+            case Border.LeftSingle | Border.RightSingle | Border.DownDouble: return '╥';
+
+            case Border.LeftDouble | Border.RightDouble | Border.TopSingle: return '╧';
+            case Border.LeftSingle | Border.RightSingle | Border.TopDouble: return '╨';
+
+            case Border.LeftDouble | Border.RightDouble | Border.TopSingle | Border.DownSingle: return '╪';
+            case Border.LeftSingle | Border.RightSingle | Border.TopDouble | Border.DownDouble: return '╫';
+            default:
+                return ' ';
+        }
+    }
+
+
+    public static Border LookupBorder(char ch)
+    {
+        switch (ch)
+        {
+            case '─': return Border.LeftSingle | Border.RightSingle;
+            case '│': return Border.TopSingle | Border.DownSingle;
+            case '┌': return Border.RightSingle | Border.DownSingle;
+            case '┐': return Border.LeftSingle | Border.DownSingle;
+            case '└': return Border.TopSingle | Border.RightSingle;
+            case '┘': return Border.LeftSingle | Border.TopSingle;
+            case '├': return Border.TopSingle | Border.DownSingle | Border.RightSingle;
+            case '┤': return Border.TopSingle | Border.DownSingle | Border.LeftSingle;
+            case '┬': return Border.LeftSingle | Border.RightSingle | Border.DownSingle;
+            case '┴': return Border.LeftSingle | Border.RightSingle | Border.TopSingle;
+            case '┼': return Border.LeftSingle | Border.RightSingle | Border.TopSingle | Border.DownSingle;
+
+            case '═': return Border.LeftDouble | Border.RightDouble;
+            case '║': return Border.TopDouble | Border.DownDouble;
+            case '╔': return Border.RightDouble | Border.DownDouble;
+            case '╗': return Border.LeftDouble | Border.DownDouble;
+            case '╚': return Border.TopDouble | Border.RightDouble;
+            case '╝': return Border.LeftDouble | Border.TopDouble;
+            case '╠': return Border.TopDouble | Border.DownDouble | Border.RightDouble;
+            case '╣': return Border.TopDouble | Border.DownDouble | Border.LeftDouble;
+            case '╦': return Border.LeftDouble | Border.RightDouble | Border.DownDouble;
+            case '╩': return Border.LeftDouble | Border.RightDouble | Border.TopDouble;
+            case '╬': return Border.LeftDouble | Border.RightDouble | Border.TopDouble | Border.DownDouble;
+
+            case '╓': return Border.RightSingle | Border.DownDouble;
+            case '╒': return Border.RightDouble | Border.DownSingle;
+
+            case '╕': return Border.LeftDouble | Border.DownSingle;
+            case '╖': return Border.LeftSingle | Border.DownDouble;
+
+            case '╘': return Border.TopSingle | Border.RightDouble;
+            case '╙': return Border.TopDouble | Border.RightSingle;
+
+            case '╛': return Border.LeftDouble | Border.TopSingle;
+            case '╜': return Border.LeftSingle | Border.TopDouble;
+
+            case '╞': return Border.TopSingle | Border.DownSingle | Border.RightDouble;
+            case '╟': return Border.TopDouble | Border.DownDouble | Border.RightSingle;
+
+            case '╡': return Border.LeftDouble | Border.TopSingle | Border.DownSingle;
+            case '╢': return Border.LeftSingle | Border.TopDouble | Border.DownDouble;
+
+            case '╤': return Border.LeftDouble | Border.RightDouble | Border.DownSingle;
+            case '╥': return Border.LeftSingle | Border.RightSingle | Border.DownDouble;
+
+            case '╧': return Border.LeftDouble | Border.RightDouble | Border.TopSingle;
+            case '╨': return Border.LeftSingle | Border.RightSingle | Border.TopDouble;
+
+            case '╪': return Border.LeftDouble | Border.RightDouble | Border.TopSingle | Border.DownSingle;
+            case '╫': return Border.LeftSingle | Border.RightSingle | Border.TopDouble | Border.DownDouble;
+            default:
+                return Border.Unknown;
+        }
+    }
+}
+
+[Flags]
+public enum Border
+{
+    Unknown = 0,
+
+    TopSingle = 0b00_00_01_00,
+    DownSingle = 0b00_01_00_00,
+    LeftSingle = 0b01_00_00_00,
+    RightSingle = 0b00_00_00_01,
+
+    TopDouble = 0b00_00_11_00,
+    DownDouble = 0b00_11_00_00,
+    LeftDouble = 0b11_00_00_00,
+    RightDouble = 0b00_00_00_11,
+}
---
+++ Tuif/Dom/Frame.cs
@@ -0,0 +1,126 @@
+namespace Tuif.Dom;
+
+public class Frame : Node
+{
+    public enum Style
+    {
+        Single,
+        Double,
+
+    }
+
+    public Style BorderStyle = Style.Single;
+    public Color BorderColor = new Color(0xFFFFFF);
+    private Node? _child;
+
+    public Frame(uint width, uint height, Node? child = null) : base(width, height)
+    {
+        _child = child;
+    }
+
+    public Frame(Node child, Style border = Style.Single) : base(child.GetWidth() + 2, child.GetHeigh() + 2)
+    {
+        _child = child;
+        BorderStyle = border;
+    }
+
+    public void SetChild(Node child)
+    {
+        _child = child;
+        SetParent(_child.GetParent());
+    }
+
+    public override void UpdateSize(uint width, uint height)
+    {
+        base.UpdateSize(width, height);
+        
+        if (_child is not null) 
+        {
+            _child.UpdateSize(_width - 2, _height - 2);
+            _child.SetPos(_posX + 1, _posY + 1);
+        }
+    }
+
+    public override bool HandleKey(ConsoleKeyInfo info, ref Node focusedNode)
+    {
+        if (_child is null) 
+        {
+            return false;
+        }
+
+        focusedNode = _child;
+        RemoveFocus();
+        focusedNode.SetFocus();
+        
+        return focusedNode.HandleKey(info, ref focusedNode);
+    }
+    public override void Render(Buffer buff)
+    {
+        char verticalCh, horizontalCh, topLCh, topRCh, bottomLCh, bottomRCh;
+
+        if (BorderStyle == Style.Single)
+        {
+            horizontalCh = '─';
+            verticalCh = '│';
+            topLCh = '┌';
+            topRCh = '┐';
+            bottomLCh = '└';
+            bottomRCh = '┘';
+        }
+        else
+        {
+            horizontalCh = '═';
+            verticalCh = '║';
+            topLCh = '╔';
+            topRCh = '╗';
+            bottomLCh = '╚';
+            bottomRCh = '╝';
+        }
+
+        uint index = _posY * buff.Width + _posX;
+        uint indexBottom = index + buff.Width * (_height - 1);
+        uint indexTopRight = index + _width - 1;
+        uint indexBottomRight = indexBottom + _width - 1;
+
+        // top and bottom
+        for (uint i = 1; i < _width - 1; i++)
+        {
+            buff.Foreground[index + i] = BorderColor;
+            buff.Data[index + i] = BorderChar.MergeChars(buff.Data[index + i], horizontalCh);
+            
+            buff.Data[indexBottom + i] = BorderChar.MergeChars(buff.Data[indexBottom + i], horizontalCh);
+            buff.Foreground[indexBottom + i] = BorderColor;
+        }
+
+        // left and right
+        for (uint i = 1; i < _height - 1; i++)
+        {
+            uint tmp = index + buff.Width * i;
+
+            buff.Foreground[tmp] = BorderColor;
+            buff.Data[tmp] = BorderChar.MergeChars(buff.Data[tmp], verticalCh);
+     
+            buff.Data[tmp + _width - 1] = BorderChar.MergeChars(buff.Data[tmp + _width - 1], verticalCh);
+            buff.Foreground[tmp + _width - 1] = BorderColor;
+        }
+
+        // corners:
+        buff.Data[index] = BorderChar.MergeChars(buff.Data[index], topLCh);
+        buff.Foreground[index] = BorderColor;
+
+        buff.Data[indexTopRight] = BorderChar.MergeChars(buff.Data[indexTopRight], topRCh);
+        buff.Foreground[indexTopRight] = BorderColor;
+
+        buff.Data[indexBottom] = BorderChar.MergeChars(buff.Data[indexBottom], bottomLCh);
+        buff.Foreground[indexBottom] = BorderColor;
+
+        buff.Data[indexBottomRight] = BorderChar.MergeChars(buff.Data[indexBottomRight], bottomRCh);
+        buff.Foreground[indexBottomRight] = BorderColor;
+
+        if (_child is not null)
+        {
+            _child.SetPos(_posX + 1, _posY + 1);
+            _child.Render(buff);
+        }
+    }
+}
